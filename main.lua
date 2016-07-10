5a76e4a4b5848f50fe88b96506172b083ba17d10
Kaixhin <design@kaixhin.com>
2016-07-10 22:52:47 +0100

    Convert SparseAE

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 -- Load dependencies
 local mnist = require 'mnist'
 local optim = require 'optim'
+local gnuplot = require 'gnuplot'
 local image = require 'image'
 
 -- Set up Torch
@@ -9,10 +10,6 @@ torch.setheaptracking(true)
 torch.setdefaulttensortype('torch.FloatTensor')
 torch.manualSeed(1)
 
--- Shared hyperparameters
-local learningRate = 0.001
-local batchSize = 600
-local epochs = 10
 
 -- Load MNIST data
 local XTrain = mnist.traindataset().data:float():div(255) -- Normalise to [0, 1]
@@ -22,7 +19,10 @@ local N = XTrain:size(1)
 -- Choose model to train
 local cmd = torch.CmdLine()
 cmd:option('-model', 'AE', 'Model: AE|SparseAE|CAE|DeepAE|ConvAE|UpconvAE|DenoisingAE|Seq2SeqAE|VAE|AdvAE')
+cmd:option('-learningRate', 0.001, 'Learning rate')
+cmd:option('-epochs', 10, 'Training epochs')
 local opt = cmd:parse(arg)
+opt.batchSize = 600 -- Currently only set up for divisors of N
 
 -- Create model
 local Model = require ('models/' .. opt.model)
@@ -56,19 +56,28 @@ end
 -- Train
 print('Training')
 autoencoder:training()
-local optimParams = {learningRate = learningRate}
+local optimParams = {learningRate = opt.learningRate}
 local __, loss
+local losses = {}
 
-for epoch = 1, epochs do
-  print('Epoch ' .. epoch .. '/' .. epochs)
-  for n = 1, N, batchSize do
+for epoch = 1, opt.epochs do
+  print('Epoch ' .. epoch .. '/' .. opt.epochs)
+  for n = 1, N, opt.batchSize do
     -- Get minibatch
-    x = XTrain:narrow(1, n, batchSize)
+    x = XTrain:narrow(1, n, opt.batchSize)
 
     -- Optimise
     __, loss = optim.adam(feval, theta, optimParams)
+    losses[#losses + 1] = loss[1]
   end
 end
+
+-- Plot training curve
+gnuplot.pngfigure('Training.png')
+gnuplot.plot('', torch.linspace(1, #losses, #losses), torch.Tensor(losses), '-')
+gnuplot.ylabel('Loss')
+gnuplot.xlabel('Batch #')
+gnuplot.plotflush()
 
 -- Test
 print('Testing')
@@ -77,4 +86,4 @@ x = XTest:narrow(1, 1, 10)
 local xHat = autoencoder:forward(x)
 
 -- Plot reconstructions
-image.save('reconstructions.png', torch.cat(image.toDisplayTensor(x, 2, 10), image.toDisplayTensor(xHat, 2, 10), 1))
+image.save('Reconstructions.png', torch.cat(image.toDisplayTensor(x, 2, 10), image.toDisplayTensor(xHat, 2, 10), 1))
--- models/SparseAE.lua
+++ models/SparseAE.lua
@@ -1,65 +1,29 @@
-local mnist = require 'mnist'
 local nn = require 'nn'
-local optim = require 'optim'
-local image = require 'image'
 
-torch.setdefaulttensortype('torch.FloatTensor')
+local Model = {}
 
-local learningRate = 0.01
-local batchSize = 600
-local codeLength = 32
-local sparsity = 10e-5
+function Model:createAutoencoder(X)
+  local featureSize = X:size(2) * X:size(3)
 
--- Load MNIST data
-local data = mnist.traindataset().data:float():div(255)
-local N = data:size(1)
-local size = data:size(2) * data:size(3)
+  -- Create encoder
+  self.encoder = nn.Sequential()
+  self.encoder:add(nn.View(-1, featureSize))
+  self.encoder:add(nn.Linear(featureSize, 32))
+  self.encoder:add(nn.ReLU(true))
 
--- Create encoder
-local encoder = nn.Sequential()
-encoder:add(nn.View(-1, size))
-encoder:add(nn.Linear(size, codeLength))
-encoder:add(nn.ReLU(true))
+  -- Create decoder
+  self.decoder = nn.Sequential()
+  self.decoder:add(nn.Linear(32, featureSize))
+  self.decoder:add(nn.Sigmoid(true))
+  self.decoder:add(nn.View(X:size(2), X:size(3)))
 
--- Create decoder
-local decoder = nn.Sequential()
-decoder:add(nn.Linear(codeLength, size))
-decoder:add(nn.Sigmoid(true))
-decoder:add(nn.View(data:size(2), data:size(3)))
+  -- Create autoencoder
+  self.autoencoder = nn.Sequential()
+  self.autoencoder:add(self.encoder)
+  self.autoencoder:add(nn.L1Penalty(1e-6))
+  self.autoencoder:add(self.decoder)
 
--- Create autoencoder
-local ae = nn.Sequential()
-ae:add(encoder)
-ae:add(nn.L1Penalty(10e-5))
-ae:add(decoder)
-
--- Get params
-local params, gradParams = ae:getParameters()
-
--- Create loss
-local crit = nn.BCECriterion()
-
--- Train
-local batch
-local feval = function(x)
-  if params ~= x then
-    params:copy(x)
-  end
-  -- Zero gradients
-  gradParams:zero()
-
-  local xHat = ae:forward(batch)
-  local loss = crit:forward(xHat, batch)
-  local gradLoss = crit:backward(xHat, batch)
-  ae:backward(batch, gradLoss)
-
-  return loss, gradParams
+  return self.autoencoder
 end
 
-local optimParams = {learningRate = learningRate}
-for n = 1, N, batchSize do
-  -- Get batch of data
-  batch = data:narrow(1, n, batchSize)
-  local __, loss = optim.adam(feval, params, optimParams)
-  print(loss[1])
-end
+return Model
